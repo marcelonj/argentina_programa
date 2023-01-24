9e4104f06371c7bc22b139b0a75e2c600542606f
Marcelo Jurado <marcelonjurado@gmail.com>
2023-01-23 21:22:53 -0300

    Se creo la funcion para obtener datos desde la API de Random User

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-fetch('https://randomuser.me/api/')
-    .then(response => response.json())
-    .then(json => console.log(json.results))
+function detectarInicio() {
+    if (document.getElementById('saludo')){
+        async function obtenerPersona(){
+            let persona = await fetch('https://randomuser.me/api/?nat=es')
+                                 .then(response => response.json())
+                                 .then(datos => datos)
+            console.log(persona.results);
+            document.getElementById('nombre').innerHTML = persona.results[0].name.first;
+        }
+        obtenerPersona();
+    }
+    else{  
+        async function obtenerPersona(){
+            let persona = await fetch('https://randomuser.me/api/?nat=es')
+                                 .then(response => response.json())
+                                 .then(datos => datos)
+            console.log(persona.results);
+            document.getElementById('nombre').innerHTML = persona.results[0].name.first;
+            document.getElementById('email_dp').innerHTML = persona.results[0].email;
+            document.getElementById('fn_dp').innerHTML = persona.results[0].dob.date;
+            document.getElementById('direccion_dp').innerHTML = persona.results[0].location.street.name + ' ' +persona.results[0].location.street.number;
+            document.getElementById('telefono_dp').innerHTML = persona.results[0].phone;
+            }
+            obtenerPersona();
+    }
+}
+
+
+
+
+detectarInicio();
+
