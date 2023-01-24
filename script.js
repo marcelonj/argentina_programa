40b4bcdd62faed5ef071177de27b2b392a8b5bed
Marcelo Jurado <marcelonjurado@gmail.com>
2023-01-23 22:10:00 -0300

    Se comentó el js y se agrego la edad a los datos personales

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/* 
+Esta función primero determina sei estamos en la página de información personal, y de ser así se conecta a la API de Random User para obtener datos de una persona e insertarlos en el DOM.
+*/
 function detectarDatosPersonales() {
     if (document.getElementById('dp')){
         async function obtenerPersona(){
@@ -9,6 +12,7 @@ function detectarDatosPersonales() {
             document.getElementById('email_dp').innerHTML = persona.results[0].email;
             let fn = persona.results[0].dob.date;
             document.getElementById('fn_dp').innerHTML = fn.substr(0, 10);
+            document.getElementById('edad_dp').innerHTML = persona.results[0].dob.age + ' años';
             document.getElementById('direccion_dp').innerHTML = persona.results[0].location.street.name + ' ' +persona.results[0].location.street.number;
             document.getElementById('telefono_dp').innerHTML = persona.results[0].phone;
             }
