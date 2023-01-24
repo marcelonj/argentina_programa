b697dec74a04b2dfbf7eb3acda06f6b2741790e3
Marcelo Jurado <marcelonjurado@gmail.com>
2023-01-23 21:34:20 -0300

    Se modifico la funcion para obtener datos desde la API de Random User y el index

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-function detectarInicio() {
-    if (document.getElementById('saludo')){
+function detectarDatosPersonales() {
+    if (document.getElementById('dp')){
         async function obtenerPersona(){
             let persona = await fetch('https://randomuser.me/api/?nat=es')
                                  .then(response => response.json())
                                  .then(datos => datos)
             console.log(persona.results);
-            document.getElementById('nombre').innerHTML = persona.results[0].name.first;
-        }
-        obtenerPersona();
-    }
-    else{  
-        async function obtenerPersona(){
-            let persona = await fetch('https://randomuser.me/api/?nat=es')
-                                 .then(response => response.json())
-                                 .then(datos => datos)
-            console.log(persona.results);
-            document.getElementById('nombre').innerHTML = persona.results[0].name.first;
+            document.getElementById('nombre').innerHTML = persona.results[0].name.first + ' ' + persona.results[0].name.last;
             document.getElementById('email_dp').innerHTML = persona.results[0].email;
-            document.getElementById('fn_dp').innerHTML = persona.results[0].dob.date;
+            let fn = persona.results[0].dob.date;
+            document.getElementById('fn_dp').innerHTML = fn.substr(0, 10);
             document.getElementById('direccion_dp').innerHTML = persona.results[0].location.street.name + ' ' +persona.results[0].location.street.number;
             document.getElementById('telefono_dp').innerHTML = persona.results[0].phone;
             }
-            obtenerPersona();
+        obtenerPersona();
     }
 }
 
 
 
 
-detectarInicio();
+detectarDatosPersonales();
 
