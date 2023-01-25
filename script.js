71ec32f100717d3d666695d7b576cc7f696ab66f
Marcelo Jurado <marcelonjurado@gmail.com>
2023-01-25 13:43:02 -0300

    Se creó la página estuios y se modifico el js

--- script.js
+++ script.js
@@ -39,8 +39,20 @@ function mostrar_ocultar(div) {
     }
 }
 
-let boton_representante = document.getElementById('boton_representante');
-boton_representante.addEventListener("click", () => mostrar_ocultar('representante'));
 
-let boton_mesero = document.getElementById('boton_mesero');
-boton_mesero.addEventListener("click", () => mostrar_ocultar('mesero'));
+
+if (document.getElementById('estudios')){
+    let boton_universidad = document.getElementById('boton_universidad');
+    boton_universidad.addEventListener("click", () => mostrar_ocultar('universidad'));
+    let boton_curoso = document.getElementById('boton_curoso');
+    boton_curoso.addEventListener("click", () => mostrar_ocultar('curso'));
+}
+else if (document.getElementById('experiencia_laboral')){
+    let boton_representante = document.getElementById('boton_representante');
+    boton_representante.addEventListener("click", () => mostrar_ocultar('representante'));
+
+    let boton_mesero = document.getElementById('boton_mesero');
+    boton_mesero.addEventListener("click", () => mostrar_ocultar('mesero'));
+}
+
+
